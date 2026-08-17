0850cccb9b06138fc605a21eaa07cb0e34cd7956
harveyspectre484-tech <harveyspectre484@gmail.com>
2026-08-18 01:45:57 +0530

    all section json

--- processpdf.py
+++ processpdf.py
@@ -53,13 +53,16 @@ BASE = Path(r"C:\Users\10459\Desktop\rag\incomeTax2025")
 #     ], check=True)
 
 # print("\nAll sections completed.")
+list2 = list(range(3,537))
 
 list1 = [19,31,36,39,46,52,58,61,63,69,70,73,193,194,201,202,204,
 206,207,208,209,210,211,214,218,227,263,265,286 ,288,322,337,352,354,393,394,402,408,423,425,437,536]
 
-for num in list1:
+list3 = list(set(list2) - set(list1))
+
+for num in list3:
     input = f"/Users/avikalchauhan/incometax2025anti/incomeTax2025/MDfileadobe/{num}section.md"
 
-    output = f"/Users/avikalchauhan/incometax2025anti/incomeTax2025/MDfileadobe/{num}section.json"
+    output = f"/Users/avikalchauhan/incometax2025anti/incomeTax2025/jsonadobe/{num}section.json"
 
     subprocess.run(["python","/Users/avikalchauhan/incometax2025anti/incomeTax2025/mdtojsonadobe.py",input, "-o", output])  
